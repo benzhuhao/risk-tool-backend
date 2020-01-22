8947907b2cd2c86f49e75862fa1f5e7caae0a22c
benzhuhao <benzhuhao1983@gmail.com>
2020-01-22 16:26:21 +0000

    add passport

--- src/app.js
+++ src/app.js
@@ -13,23 +13,28 @@ const db_Url = process.env.DB_URL || 'dms-risk-tool-0f7pd.mongodb.net';
 const db_Username = process.env.DB_USERNAME || 'dbUser';
 const db_Password = process.env.DB_PASSWORD || 'dbUser';
 const db_Collection = process.env.DB_COLLECTION || 'test';
+const db_port = process.env.DB_PORT;
 const port = process.env.PORT || 4000;
 
+app.use(passport.initialize());
 
-mongoose.connect(`mongodb+srv://${db_Username}:${db_Password}@${db_Url}/${db_Collection}?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb://${db_Username}:${db_Password}@${db_Url}:${db_port}/${db_Collection}`,
             {
+                useCreateIndex: true,
                 useNewUrlParser: true,
+                useUnifiedTopology: true,
             }
         )
         .then((req, res)=> {
-            console.log('Mongo DB connect succesful.', res.data);
+            console.log('Mongo DB connect succesful.');
         })
         .catch(err => {
             console.log('Mongo DB connect error,', err);
         });
-
+        
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api', (req, res) => {
     res.json({
